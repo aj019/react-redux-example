e430c0e4d4e9d0ff0dd6a0bd585eba18579a8dd9
Anuj Gupta <junagupta19@gmail.com>
2020-03-01 08:31:49 +0530

    Created a class based compont named ComponentA inside components which prints out a value and has a button . Replaced App components with ComonentA

---
+++ src/components/ComponentA/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class ComponentA extends Component {
+  render() {
+    return (
+      <div>
+        <p>The value is 0</p>
+        <button>Add</button>
+      </div>
+    )
+  }
+}
+
+export default ComponentA
